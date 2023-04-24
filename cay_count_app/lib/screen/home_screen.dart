98194d834d37f35ebc6b2d4ee2297acdfd4dfcca
Syeong0013 <dnwneotmxk77@hanmail.net>
2023-04-25 01:10:15 +0900

    230424_sykim_day count app 완성

--- cay_count_app/lib/screen/home_screen.dart
+++ cay_count_app/lib/screen/home_screen.dart
@@ -11,41 +11,73 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color.getAlphaFromOpacity('0xFFABABAB'),
       body: SafeArea(
+        bottom: false,
         child: Container(
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              const Text(
-                'D-day',
-                style: TextStyle(
-                  fontFamily: 'noto',
-                  fontSize: 80,
-                ),
-              ),
-              const Text(
-                '2022-203-02',
-                style: TextStyle(
-                  fontFamily: 'noto',
-                  fontSize: 30,
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.favorite),
-              ),
-              const Text(
-                'D+1',
-                style: TextStyle(
-                  fontFamily: 'noto',
-                  fontSize: 25,
-                ),
-              ),
+              _TopPart(),
+              _BottomPart(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Text(
+            'D-day',
+            style: TextStyle(
+              fontFamily: 'noto',
+              fontSize: 80,
+            ),
+          ),
+          const Text(
+            '2022.03.02',
+            style: TextStyle(
+              fontFamily: 'noto',
+              fontSize: 30,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.favorite),
+            iconSize: 60.0,
+            color: Colors.red,
+          ),
+          const Text(
+            'D+1',
+            style: TextStyle(
+              fontFamily: 'noto',
+              fontSize: 35,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Image.asset('asset/img/image.jpg'),
     );
   }
 }
