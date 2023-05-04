ded183d8a227c99150b915ebbd268e44880d0ebc
Syeong0013 <dnwneotmxk77@hanmail.net>
2023-05-04 23:54:52 +0900

    230504_sykim_네비게이션 학습

---
+++ navigation/lib/screen/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:navigation/layout/main_layout.dart';
+import 'package:navigation/screen/route_one_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'Home Screen',
+      children: [
+        ElevatedButton(
+          // 다음 라우트로 이동
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => RouteOneScreen(),
+            ));
+          },
+          child: Text('버튼'),
+        )
+      ],
+    );
+  }
+}
